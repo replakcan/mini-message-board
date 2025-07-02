231613f596c1762e4c905493221d805bb23d7904
replakcan <alper.makcan@gmail.com>
2025-07-02 21:35:39 +0300

    Populates db with seed data

---
+++ db/populatedb.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const { Client } = require('pg')
+
+const SQL = `CREATE TABLE IF NOT EXISTS messages (
+id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, author_name VARCHAR ( 20 ), message VARCHAR ( 255 )
+);
+
+INSERT INTO messages (author_name, message)
+VALUES ('Alper Akcan', 'hello from the other side'), ('Odin', 'selamin hello'), ('Deamon Lillard', 'Light weight babeee'); 
+`
+
+async function main() {
+  console.log('seeding...')
+  const client = new Client({
+    connectionString: `postgresql://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@localhost:5432/${process.env.DATABASE}`,
+  })
+
+  await client.connect()
+  await client.query(SQL)
+  await client.end()
+
+  console.log('done')
+}
+
+main()
